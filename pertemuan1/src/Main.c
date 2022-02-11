a86381e366fd78c963a23f7ca68bffbfa49b1875
Muhammad Ikhsan <88651883+Ikhsans4@users.noreply.github.com>
2022-02-11 15:01:33 +0700

    pertemuan 1

---
+++ pertemuan1/src/Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // deklarasi variabel
+    int angka = 10;
+    float a = 1.5;
+
+    printf("bilangan bulat = %d\n", angka);
+    printf("bilangan bulat = %f", a);
+
+    return 0;
+}
